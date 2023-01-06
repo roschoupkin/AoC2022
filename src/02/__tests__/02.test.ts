aa4815a426bd6cfa97dbba6d5ea2de746497f065
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2023-01-06 21:59:03 +0300

    test: update tests execution

--- src/02/__tests__/02.test.ts
+++ src/02/__tests__/02.test.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { one } from '../solution.0';
 import { two } from '../solution.1';
 
-describe('01', () => {
+describe('02', () => {
   test('part one', () => {
     expect(one(path.join(__dirname, '..', 'example.txt'))).toBe(15);
     expect(one(path.join(__dirname, '..', 'puzzle.txt'))).toBe(12645);
